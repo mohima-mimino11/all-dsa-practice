d6492d183fa0c3cfccb9924eba513596cbf941ba
mohima-mimino11 <mohimahoque22@gmail.com>
2025-10-24 02:06:26 +0600

    dfs concept added

---
+++ algorithms/day-76/dfs.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adjList[1005];
+bool vis[1005];
+void dfs(int src){
+  cout << src << " ";
+  vis[src] = true;
+  for(int child: adjList[src]){
+    if(!vis[child]){
+        dfs(child);
+    }
+  }
+}
+int main(){
+  int n, e;
+  cin >> n >> e;
+  while(e--){
+    int a, b;
+    cin >> a >> b;
+    adjList[a].push_back(b);
+    adjList[b].push_back(a);
+  }
+  memset(vis, false, sizeof(vis));
+  dfs(0);
+  return 0;
+}
